a05bda4103a5912892bf6b2338c34666bf62ca4a
Victor Lu <75279620+victlu@users.noreply.github.com>
2021-03-17 14:57:58 -0700

    Add additional Bench for passing labels

--- example3/Benchmarks/LabelPassingBench.cs
+++ example3/Benchmarks/LabelPassingBench.cs
@@ -14,20 +14,22 @@ BenchmarkDotNet=v0.12.1, OS=Windows 10.0.19042
 Intel Core i7-1065G7 CPU 1.30GHz, 1 CPU, 8 logical and 4 physical cores
 .NET Core SDK=5.0.201
   [Host]     : .NET Core 5.0.4 (CoreCLR 5.0.421.11614, CoreFX 5.0.421.11614), X64 RyuJIT
-  Job-ZJXXBO : .NET Core 5.0.4 (CoreCLR 5.0.421.11614, CoreFX 5.0.421.11614), X64 RyuJIT
+  Job-HUOAJJ : .NET Core 5.0.4 (CoreCLR 5.0.421.11614, CoreFX 5.0.421.11614), X64 RyuJIT
 
 IterationCount=20  LaunchCount=5  WarmupCount=5
 
 |                Method |        Mean |     Error |    StdDev |      Median |  Gen 0 | Gen 1 | Gen 2 | Allocated |
 |---------------------- |------------:|----------:|----------:|------------:|-------:|------:|------:|----------:|
-|           AddByValues |    53.12 ns |  0.389 ns |  1.136 ns |    53.32 ns | 0.0114 |     - |     - |      48 B |
-|       AddByValueTuple |    62.24 ns |  1.043 ns |  3.060 ns |    61.39 ns | 0.0172 |     - |     - |      72 B |
-|      Add2ByValueTuple |    60.38 ns |  0.531 ns |  1.567 ns |    60.48 ns | 0.0172 |     - |     - |      72 B |
-|       AddByDictionary |   170.29 ns |  1.154 ns |  3.384 ns |   170.60 ns | 0.0648 |     - |     - |     272 B |
-|      AddByLargeValues |   343.54 ns |  2.424 ns |  7.146 ns |   343.27 ns | 0.0439 |     - |     - |     184 B |
-|  AddByLargeValueTuple |   380.91 ns |  2.660 ns |  7.633 ns |   379.75 ns | 0.0820 |     - |     - |     344 B |
-| Add2ByLargeValueTuple |   380.08 ns |  2.958 ns |  8.676 ns |   380.62 ns | 0.0820 |     - |     - |     344 B |
-|  AddByLargeDictionary | 1,161.07 ns | 11.926 ns | 34.409 ns | 1,162.90 ns | 0.5093 |     - |     - |    2136 B |
+|           AddByValues |    56.67 ns |  0.974 ns |  2.811 ns |    55.81 ns | 0.0114 |     - |     - |      48 B |
+|       AddByValueTuple |    66.53 ns |  2.225 ns |  6.454 ns |    64.34 ns | 0.0172 |     - |     - |      72 B |
+|           AddByStruct |    69.02 ns |  1.408 ns |  4.086 ns |    67.62 ns | 0.0172 |     - |     - |      72 B |
+|      AddByIEnumStruct |   150.56 ns |  0.941 ns |  2.730 ns |   150.81 ns | 0.0401 |     - |     - |     168 B |
+|       AddByDictionary |   208.17 ns |  2.874 ns |  8.292 ns |   210.07 ns | 0.0648 |     - |     - |     272 B |
+|      AddByLargeValues |   400.05 ns |  2.708 ns |  7.898 ns |   398.16 ns | 0.0439 |     - |     - |     184 B |
+|  AddByLargeValueTuple |   441.10 ns |  5.040 ns | 14.460 ns |   435.63 ns | 0.0820 |     - |     - |     344 B |
+|      AddByLargeStruct |   436.75 ns |  3.916 ns | 11.108 ns |   437.51 ns | 0.0820 |     - |     - |     344 B |
+| AddByLargeIEnumStruct |   941.86 ns | 12.256 ns | 34.967 ns |   926.68 ns | 0.2708 |     - |     - |    1136 B |
+|  AddByLargeDictionary | 1,335.59 ns | 20.379 ns | 57.811 ns | 1,323.23 ns | 0.5093 |     - |     - |    2136 B |
 */
 
 namespace MyBenchmark
@@ -77,15 +79,29 @@ namespace MyBenchmark
         }
 
         [Benchmark]
-        public int Add2ByValueTuple()
+        public int AddByStruct()
         {
-            var values = new (string, string)[] {
-                ( "host", "host1" ),
-                ( "location", "location1" ),
-                ( "tenantid", "tenantid1" ),
+            var values = new NameValue[] {
+                new NameValue( "host", "host1" ),
+                new NameValue( "location", "location1" ),
+                new NameValue( "tenantid", "tenantid1" ),
             };
 
-            this.counter1.Add2(values);
+            this.counter1.Add(values);
+
+            return 10;
+        }
+
+        [Benchmark]
+        public int AddByIEnumStruct()
+        {
+            var values = new List<NameValue> {
+                new NameValue( "host", "host1" ),
+                new NameValue( "location", "location1" ),
+                new NameValue( "tenantid", "tenantid1" ),
+            };
+
+            this.counter1.Add(values);
 
             return 10;
         }
@@ -152,32 +168,63 @@ namespace MyBenchmark
         }
 
         [Benchmark]
-        public int Add2ByLargeValueTuple()
+        public int AddByLargeStruct()
         {
-            var values = new (string, string)[] {
-                ( "col01", "val01" ),
-                ( "col02", "val02" ),
-                ( "col03", "val03" ),
-                ( "col04", "val04" ),
-                ( "col05", "val05" ),
-                ( "col06", "val06" ),
-                ( "col07", "val07" ),
-                ( "col08", "val08" ),
-                ( "col09", "val09" ),
-                ( "col10", "val00" ),
-                ( "col11", "val11" ),
-                ( "col12", "val12" ),
-                ( "col13", "val13" ),
-                ( "col14", "val14" ),
-                ( "col15", "val15" ),
-                ( "col16", "val16" ),
-                ( "col17", "val17" ),
-                ( "col18", "val18" ),
-                ( "col19", "val19" ),
-                ( "col20", "val20" ),
+            var values = new NameValue[] {
+                new NameValue( "col01", "val01" ),
+                new NameValue( "col02", "val02" ),
+                new NameValue( "col03", "val03" ),
+                new NameValue( "col04", "val04" ),
+                new NameValue( "col05", "val05" ),
+                new NameValue( "col06", "val06" ),
+                new NameValue( "col07", "val07" ),
+                new NameValue( "col08", "val08" ),
+                new NameValue( "col09", "val09" ),
+                new NameValue( "col10", "val00" ),
+                new NameValue( "col11", "val11" ),
+                new NameValue( "col12", "val12" ),
+                new NameValue( "col13", "val13" ),
+                new NameValue( "col14", "val14" ),
+                new NameValue( "col15", "val15" ),
+                new NameValue( "col16", "val16" ),
+                new NameValue( "col17", "val17" ),
+                new NameValue( "col18", "val18" ),
+                new NameValue( "col19", "val19" ),
+                new NameValue( "col20", "val20" ),
             };
 
-            this.counter2.Add2(values);
+            this.counter1.Add(values);
+
+            return 10;
+        }
+
+        [Benchmark]
+        public int AddByLargeIEnumStruct()
+        {
+            var values = new List<NameValue> {
+                new NameValue( "col01", "val01" ),
+                new NameValue( "col02", "val02" ),
+                new NameValue( "col03", "val03" ),
+                new NameValue( "col04", "val04" ),
+                new NameValue( "col05", "val05" ),
+                new NameValue( "col06", "val06" ),
+                new NameValue( "col07", "val07" ),
+                new NameValue( "col08", "val08" ),
+                new NameValue( "col09", "val09" ),
+                new NameValue( "col10", "val00" ),
+                new NameValue( "col11", "val11" ),
+                new NameValue( "col12", "val12" ),
+                new NameValue( "col13", "val13" ),
+                new NameValue( "col14", "val14" ),
+                new NameValue( "col15", "val15" ),
+                new NameValue( "col16", "val16" ),
+                new NameValue( "col17", "val17" ),
+                new NameValue( "col18", "val18" ),
+                new NameValue( "col19", "val19" ),
+                new NameValue( "col20", "val20" ),
+            };
+
+            this.counter1.Add(values);
 
             return 10;
         }
@@ -240,19 +287,29 @@ namespace MyBenchmark
         {
             dict.Clear();
 
-            for (int i = 0; i < labels.Length; i++)
+            foreach (var label in labels)
             {
-                dict[labels[i].name] = labels[i].value;
+                dict[label.name] = label.value;
             }
         }
 
-        public void Add2(params (string name, string value)[] labels)
+        public void Add(NameValue[] labels)
         {
             dict.Clear();
 
             foreach (var label in labels)
             {
-                dict[label.name] = label.value;
+                dict[label.Name] = label.Value;
+            }
+        }
+
+        public void Add(IEnumerable<NameValue> labels)
+        {
+            dict.Clear();
+
+            foreach (var label in labels)
+            {
+                dict[label.Name] = label.Value;
             }
         }
 
@@ -264,6 +321,18 @@ namespace MyBenchmark
             {
                 dict[label.Key] = label.Value;
             }
+        }
+    }
+
+    public struct NameValue
+    {
+        public string Name;
+        public string Value;
+
+        public NameValue(string name, string value)
+        {
+            Name = name;
+            Value = value;
         }
     }
 }
