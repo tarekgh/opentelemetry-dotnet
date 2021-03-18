a5ec715bdb710d7224ecc2d87023179b9a35abbe
Victor Lu <75279620+victlu@users.noreply.github.com>
2021-03-18 10:43:46 -0700

    Update result from LabelPassing bench

--- example3/Benchmarks/LabelPassingBench.cs
+++ example3/Benchmarks/LabelPassingBench.cs
@@ -14,31 +14,31 @@ BenchmarkDotNet=v0.12.1, OS=Windows 10.0.19042
 Intel Core i7-1065G7 CPU 1.30GHz, 1 CPU, 8 logical and 4 physical cores
 .NET Core SDK=5.0.201
   [Host]     : .NET Core 5.0.4 (CoreCLR 5.0.421.11614, CoreFX 5.0.421.11614), X64 RyuJIT
-  Job-BKDMDV : .NET Core 5.0.4 (CoreCLR 5.0.421.11614, CoreFX 5.0.421.11614), X64 RyuJIT
+  Job-IAQTAE : .NET Core 5.0.4 (CoreCLR 5.0.421.11614, CoreFX 5.0.421.11614), X64 RyuJIT
 
-IterationCount=20  LaunchCount=5  WarmupCount=5
+IterationCount=20  LaunchCount=3  WarmupCount=3
 
-|                         Method |        Mean |     Error |    StdDev |      Median |  Gen 0 | Gen 1 | Gen 2 | Allocated |
-|------------------------------- |------------:|----------:|----------:|------------:|-------:|------:|------:|----------:|
-|                AddByValueArray |    56.02 ns |  0.639 ns |  1.793 ns |    56.18 ns | 0.0114 |     - |     - |      48 B |
-|            AddByNameValueArray |    59.65 ns |  0.438 ns |  1.208 ns |    59.29 ns | 0.0172 |     - |     - |      72 B |
-|             AddByColValueArray |    56.44 ns |  1.040 ns |  2.999 ns |    55.27 ns | 0.0114 |     - |     - |      48 B |
-|           AddByValueTupleArray |    68.47 ns |  1.994 ns |  5.722 ns |    68.38 ns | 0.0172 |     - |     - |      72 B |
-|               AddByStructArray |    74.85 ns |  1.369 ns |  3.907 ns |    74.77 ns | 0.0172 |     - |     - |      72 B |
-|          AddByStructEnumerator |   152.75 ns |  1.014 ns |  2.893 ns |   152.47 ns | 0.0401 |     - |     - |     168 B |
-|                AddByDictionary |   196.10 ns |  1.182 ns |  3.334 ns |   195.50 ns | 0.0648 |     - |     - |     272 B |
-|           LargeAddByValueArray |   372.22 ns |  4.989 ns | 13.988 ns |   374.63 ns | 0.0439 |     - |     - |     184 B |
-|       LargeAddByNameValueArray |   468.78 ns | 25.514 ns | 71.543 ns |   432.05 ns | 0.0820 |     - |     - |     344 B |
-|        LargeAddByColValueArray |   368.03 ns |  6.280 ns | 17.817 ns |   360.75 ns | 0.0439 |     - |     - |     184 B |
-| LargeAddByLargeValueTupleArray |   414.58 ns | 20.140 ns | 59.384 ns |   385.98 ns | 0.0820 |     - |     - |     344 B |
-|          LargeAddByStructArray |   428.95 ns |  9.260 ns | 26.118 ns |   421.31 ns | 0.0820 |     - |     - |     344 B |
-|     LargeAddByStructEnumerator |   882.29 ns |  7.890 ns | 22.510 ns |   881.87 ns | 0.2708 |     - |     - |    1136 B |
-|           LargeAddByDictionary | 1,144.69 ns |  6.693 ns | 18.546 ns | 1,140.56 ns | 0.5093 |     - |     - |    2136 B |
+|                         Method |        Mean |     Error |    StdDev |  Gen 0 | Gen 1 | Gen 2 | Allocated |
+|------------------------------- |------------:|----------:|----------:|-------:|------:|------:|----------:|
+|                AddByValueArray |    55.88 ns |  1.507 ns |  3.212 ns | 0.0114 |     - |     - |      48 B |
+|            AddByNameValueArray |    61.71 ns |  0.591 ns |  1.322 ns | 0.0172 |     - |     - |      72 B |
+|             AddByColValueArray |    65.89 ns |  0.785 ns |  1.690 ns | 0.0229 |     - |     - |      96 B |
+|           AddByValueTupleArray |    61.71 ns |  0.775 ns |  1.685 ns | 0.0172 |     - |     - |      72 B |
+|               AddByStructArray |    61.65 ns |  0.692 ns |  1.504 ns | 0.0172 |     - |     - |      72 B |
+|          AddByStructEnumerator |   136.72 ns |  1.433 ns |  3.176 ns | 0.0401 |     - |     - |     168 B |
+|                AddByDictionary |   172.74 ns |  1.662 ns |  3.684 ns | 0.0648 |     - |     - |     272 B |
+|           LargeAddByValueArray |   349.03 ns |  3.215 ns |  7.191 ns | 0.0439 |     - |     - |     184 B |
+|       LargeAddByNameValueArray |   374.21 ns |  3.991 ns |  8.760 ns | 0.0820 |     - |     - |     344 B |
+|        LargeAddByColValueArray |   385.08 ns |  3.721 ns |  8.168 ns | 0.0877 |     - |     - |     368 B |
+| LargeAddByLargeValueTupleArray |   378.16 ns |  3.692 ns |  8.182 ns | 0.0820 |     - |     - |     344 B |
+|          LargeAddByStructArray |   389.94 ns |  5.803 ns | 12.859 ns | 0.0820 |     - |     - |     344 B |
+|     LargeAddByStructEnumerator |   838.40 ns | 11.456 ns | 25.386 ns | 0.2708 |     - |     - |    1136 B |
+|           LargeAddByDictionary | 1,130.51 ns | 10.742 ns | 23.352 ns | 0.5093 |     - |     - |    2136 B |
 */
 
 namespace MyBenchmark
 {
-    [SimpleJob(launchCount: 5, warmupCount: 5, targetCount: 20)]
+    [SimpleJob(launchCount: 3, warmupCount: 3, targetCount: 20)]
     [MemoryDiagnoser]
     public class LabelPassingBench
     {
@@ -91,13 +91,20 @@ namespace MyBenchmark
         [Benchmark]
         public int AddByColValueArray()
         {
+            // This can be cached
+            var names = new string[] {
+                "host",
+                "location",
+                "tenantid"
+                };
+
             var values = new string[] {
                 "host1",
                 "location1",
                 "tenantid1"
                 };
 
-            this.counter1.Add(this.colnames1, values);
+            this.counter1.Add(names, values);
 
             return 10;
         }
@@ -226,6 +233,30 @@ namespace MyBenchmark
         [Benchmark]
         public int LargeAddByColValueArray()
         {
+            // This can be cached
+            var names = new string[] {
+                "col01",
+                "col02",
+                "col03",
+                "col04",
+                "col05",
+                "col06",
+                "col07",
+                "col08",
+                "col09",
+                "col10",
+                "col11",
+                "col12",
+                "col13",
+                "col14",
+                "col15",
+                "col16",
+                "col17",
+                "col18",
+                "col19",
+                "col20",
+                };
+
             var values = new string[] {
                 "val01",
                 "val02",
@@ -249,7 +280,7 @@ namespace MyBenchmark
                 "val20",
             };
 
-            this.counter2.Add(this.colnames2, values);
+            this.counter2.Add(names, values);
 
             return 10;
         }
